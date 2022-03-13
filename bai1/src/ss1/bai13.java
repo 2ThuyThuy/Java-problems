79c3a7e7958ec48c5f62856b97d4ff0e4a6c6bd0
htmltua <thuynguyen2462k1@gmail.com>
2022-03-13 09:23:26 +0700

    hello

--- bai1/src/ss1/bai13.java
+++ bai1/src/ss1/bai13.java
@@ -1,11 +1,34 @@
 package ss1;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+
 
 public class bai13 {
     // Tạo một mảng n chiều
-    public static void main(String[] args) {
 
+    private Object[] array; // internal representation of the N-dimensional array
+    private int[] dimensions; // dimensions of the array
+    private int[] multipliers; // used to calculate the index in the internal array
+
+    bai13(int... dimensions) {
+        int arraySize = 1;
+
+        multipliers = new int[dimensions.length];
+        for (int idx = dimensions.length - 1; idx >= 0; idx--) {
+            multipliers[idx] = arraySize;
+            arraySize *= dimensions[idx];
+        }
+        array = new Object[arraySize];
+        this.dimensions = dimensions;
     }
+    public Object get(int... indices) {
+        assert indices.length == dimensions.length;
+        int internalIndex = 0;
+
+        for (int idx = 0; idx < indices.length; idx++) {
+            internalIndex += indices[idx] * multipliers[idx];
+        }
+        return array[internalIndex];
+    }
+
+
 }
--- bai1/src/ss1/bai9.java
+++ bai1/src/ss1/bai9.java
@@ -1,16 +1,18 @@
 package ss1;
 
+import java.util.Scanner;
+
 public class bai9 {
     // tìm tất cả các mảng nguyên (int) có tổng giá trị các phần tử bằng
     // giá trị đã cho bất kì
     // các mảng khác nhau được xem là khác số phần tử, chứ ko quan tâm đến vị trí
 
-    private static void print(int sum, int start) {
-
+    private static void print(int sum, int start, int a[], int m) {
         for(int i=start; i<= sum/2; i++) {
+            for(int j=0; j<m; j++)
+                System.out.printf("%d ",a[j]);
             System.out.printf("%d %d\n",i,sum-i);
         }
-
     }
 
     private static void solve(int n, int k) {
@@ -19,14 +21,33 @@ public class bai9 {
             return;
         }
         if(k == 2) {
-            print(n, 1);
+            for(int i=1; i<= n/2; i++)
+                System.out.printf("%d %d\n",i,n-i);
             return;
         }
-
+        int m = k-2,sum=n-m,first=m;
+        int []a = new int[m];
+        for(int i=0; i<m; i++) {
+            a[i]=1;
+        }
+        int i=m-1;
+        while (first+2*a[m-1] <= n) {
+            print(sum,a[i],a,m);
+            a[i]++;
+            first++;
+            sum--;
+            i--;
+            if(i == -1) i=m-1;
+        }
     }
 
     public static void main(String[] args) {
-
-        solve(7,2);
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Nhập n: ");
+        int n = sc.nextInt();
+        System.out.println("Các mảng có tổng bằng n và số phần tử bé thua hoặc bằng n là:");
+        for(int i=1; i<=n; i++){
+            solve(n,i);
+        }
     }
 }
